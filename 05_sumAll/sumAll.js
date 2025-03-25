9f25571557e8b17f490add02486f9aa3a60adb0b
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-03-25 15:39:37 -0400

    Completing 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) return "ERROR";
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+  let min;
+  let max;
+  if (a < b) {
+    min = a;
+    max = b;
+  } else {
+    min = b;
+    max = a;
+  }
+
+  let debugStr = "";
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    debugStr += i == min ? `${i} ` : `+ ${i} `;
+
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
