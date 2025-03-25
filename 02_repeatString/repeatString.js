3245ad04b7fddc4902726637ca08c2880a5669b8
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-03-24 21:25:16 -0400

    Completing 03_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  if (num < 0) return "ERROR";
 
+  let finalStr = "";
+
+  let i = 0;
+  while (i < num) {
+    finalStr += str;
+
+    i++;
+  }
+
+  return finalStr;
 };
 
 // Do not edit below this line
