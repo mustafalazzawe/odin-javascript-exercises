2139eb04b44824f48281b0be18abcdc1a9a32df2
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-03-25 15:55:53 -0400

    Completing 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // year is divisilbe by 4 => yes
+  // unless year is divisible by 100 => no
+  // unless year is divisible by 400 => yes
 
+  if (year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+leapYears(1600);
 
 // Do not edit below this line
 module.exports = leapYears;
