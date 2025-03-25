fad400237bd36674cab0209c32fc68f5e0897766
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-03-24 21:57:09 -0400

    Completing 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  const tempArr = str.split("");
+  const reverseArr = tempArr.reverse();
 
+  return reverseArr.join("");
 };
 
 // Do not edit below this line
