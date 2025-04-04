c16ca71cb0015d523a9a1a18b3f0852a242d813e
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-04-04 10:34:23 -0400

    Completing 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleaned = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("")
+    .join("");
+  const backwards = cleaned.split("").reverse().join("");
 
+  return cleaned === backwards;
+};
+
+const palindromesTwo = function (str) {
+  let cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  let i = 0;
+  let j = str.length - 1;
+
+  while (i < j) {
+    if (cleaned[i] !== cleaned[j]) {
+      return false;
+    }
+
+    i++;
+    j--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
