3f57e872f26ad021ece469e03f683175ab2d93ba
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-04-04 11:11:23 -0400

    Completing 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num !== "number") num = +num;
 
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+
+  let f1 = 0;
+  let f2 = 1;
+
+  let curr;
+  for (let i = 0; i < num; i++) {
+    curr = f1 + f2;
+
+    f2 = f1;
+    f1 = curr;
+  }
+
+  console.log(curr);
+
+  return curr;
 };
 
 // Do not edit below this line
